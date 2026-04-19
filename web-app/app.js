2a252f3d7cd63967403637dd40e33c3005a596f1
galiana17 <galianacoronado@gmail.com>
2026-04-19 16:04:24 -0700

    save local changes before rebase

--- web-app/app.js
+++ web-app/app.js
@@ -679,12 +679,3 @@ function toggleRegisterPassword() {
     passwordField.type = "password";
   }
 }
-function togglePassword() {
-  const passwordField = document.getElementById("password");
-
-  if (passwordField.type === "password") {
-    passwordField.type = "text";
-  } else {
-    passwordField.type = "password";
-  }
-}
