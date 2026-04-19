f205b9872a6db6c7fa24143f247cef565ea99b91
tgafford <travisgafford@gmail.com>
2026-04-19 17:30:57 -0400

    added /dashboard-data route to the backend so that dashboard displays
    forgot password

--- app/__init__.py
+++ app/__init__.py
@@ -4,46 +4,42 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
-#create extensions
 db = SQLAlchemy()
 login_manager = LoginManager()
 bcrypt = Bcrypt()
 
 def create_app():
-    #create the Flask app
     app = Flask(__name__)
-    CORS(app, supports_credentials=True)
 
-    #basic config
+    #config
     app.config['SECRET_KEY'] = 'your_secret_key'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///planner.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
-    app.config['SESSION_COOKIE_SECURE'] = False
+    #update
+    app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+    app.config['SESSION_COOKIE_SECURE'] = False  #oK for local dev
 
+    #update
     CORS(app,
          supports_credentials=True,
-         origins=["http://localhost:5500"])
+         origins=[
+             "http://localhost:8000",  #python http.server
+             "http://127.0.0.1:8000"
+         ])
 
-
-    #connect extensions to app
     db.init_app(app)
     login_manager.init_app(app)
     bcrypt.init_app(app)
 
-    #tells Flask-Login where to send users if not logged in
     login_manager.login_view = 'main.login'
 
-    #import User model
     from .models import User
 
-    #this function loads a user from their ID
     @login_manager.user_loader
     def load_user(user_id):
         return db.session.get(User, int(user_id))
 
-    #register routes...connect routes.py
     from .routes import main
     app.register_blueprint(main)
 
