5b0be58099d64653c18f3b3cbffa4c870e1b5be6
tgafford <travisgafford@gmail.com>
2026-04-19 19:51:08 -0400

    .

--- web-app/app.js
+++ web-app/app.js
@@ -679,3 +679,11 @@ function toggleRegisterPassword() {
     passwordField.type = "password";
   }
 }
+function togglePassword() {
+  const passwordField = document.getElementById("password");
+
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+  } else {
+    passwordField.type = "password";
+  }
